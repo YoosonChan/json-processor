50aefa03035eec477c0007214c6e02b79b93159b
yoosonchan <yoosonchan@163.com>
2023-10-20 22:09:39 +0800

    fix: git page build fail

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   // fix git page build bug: https://github.com/nuxt/nuxt/issues/22159#issuecomment-1639851186
-  // nitro: {
-  //   prerender: {
-  //     failOnError: false,
-  //   },
-  // },
+  nitro: {
+    prerender: {
+      failOnError: false,
+    },
+  },
   // routeRules: {
   //   '/': {
   //     ssr: false,
